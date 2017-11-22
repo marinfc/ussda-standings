6e16e74de6ca9cf5edbe3c950849d7cb37e78b1b
marinfc <marinfc@robinett.org>
2017-11-22 11:06:02 -0800

    Now calculates standings

--- main.go
+++ main.go
@@ -7,16 +7,29 @@ import (
 	"net/http"
 	"io"
 	"regexp"
+	//"time"
 )
 
 // types have to be strings because results contain non integer values
 type game struct {
-	ID string `json:"id"`
-	HomeID string `json:"homeId"`
-	AwayID string `json:"awayId"`
-	HomeScore string `json:"homeScore"`
-	AwayScore string `json:"awayScore"`
+	ID			string 		`json:"id"`
+	HomeTeamID 	string 		`json:"homeId"`
+	AwayTeamID 	string 		`json:"awayId"`
+	HomeScore 	string 		`json:"homeScore"`
+	AwayScore 	string 		`json:"awayScore"`
+	HomeClubID 	string 		`json:"homeClub"`
+	AwayClubID 	string 		`json:"awayClub"`
+	HomeDivisionID string	`json:"homeDivision"`
+	AwayDivisionID string	`json:"awayDivision"`
+	//StartDate 	time.Time 	`json:"startDate"`
+	IsPlayed	string		`json:"isPlayed"`
 }
+
+const marinFC = "3139"
+// 4000300 U14
+// 4000301 U12
+// 4000302 U13
+
 
 /*
 {"id":"4154357",
@@ -30,13 +43,37 @@ type game struct {
 	"homeName":"Ballistic United SC U-12",
 	"homeScore":"0",
 	"awayName":"Placer United SC U-12",
-	"awayScore":"0","isPlayed":null,
+	"awayScore":"0",
+	"isPlayed":null,
 	"isCancelled":false,
 	"locationName":"Val Vista - Field 1",
 	"locationAddress":"7350 Johnson Dr.",
-	"locationAddress2":null,"locationCity":"Pleasanton",
-	"locationState":"CA","locationZip":"94588","locationParentName":null,"jerseyColorsAreEnabled":"0","jerseyHaveBottom":"0","jerseyHavePattern":"0","homeTopColor1":null,"homeTopColor2":null,"homeBottomColor":null,"homeAgeGroup":"449475","homeOutsideClubName":null,"homeDivision":"130811","homeClub":"3129","awayTopColor1":null,"awayTopColor2":null,"awayBottomColor":null,"awayAgeGroup":"449475","awayOutsideClubName":null,"frontEndScores":0,"hideGameReports":1,"awayDivision":"130811","awayClub":"3143"
+	"locationAddress2":null,
+	"locationCity":"Pleasanton",
+	"locationState":"CA",
+	"locationZip":"94588",
+	"locationParentName":null,
+	"jerseyColorsAreEnabled":"0",
+	"jerseyHaveBottom":"0",
+	"jerseyHavePattern":"0",
+	"homeTopColor1":null,
+	"homeTopColor2":null,
+	"homeBottomColor":null,
+	"homeAgeGroup":"449475",
+	"homeOutsideClubName":null,
+	"homeDivision":"130811",
+	"homeClub":"3129",
+	"awayTopColor1":null,
+	"awayTopColor2":null,
+	"awayBottomColor":null,
+	"awayAgeGroup":"449475",
+	"awayOutsideClubName":null,
+	"frontEndScores":0,
+	"hideGameReports":1,
+	"awayDivision":"130811",
+	"awayClub":"3143"
 */
+
 type club struct {
 	ID int `json:"id"`
 	Name string `json:"name"`
@@ -45,6 +82,17 @@ type club struct {
 type division struct {
 	ID int `json:"id"`
 	Name string `json:"name"`
+}
+
+type standing struct {
+	DivisionID string
+	ClubID string
+	TeamID string
+	Wins int
+	Ties int
+	Losses int
+	Points int
+	Games []game
 }
 
 func getScriptCode(r io.Reader) string {
@@ -113,6 +161,79 @@ func parseJSONs(gamesJSON string, clubsJSON string, divisionsJSON string) ([]gam
 	return games, clubs, divisions
 }
 
+func getStanding(standings map[string]*standing, g game, isAway bool) *standing {
+
+	var ok bool
+	var val *standing
+	var teamID string
+	if isAway {
+		teamID = g.AwayTeamID
+	} else {
+		teamID = g.HomeTeamID
+	}
+
+	if val, ok = standings[teamID]; !ok {
+		val = &standing{}
+		val.TeamID = teamID
+		if isAway {
+			val.ClubID = g.AwayClubID
+			val.DivisionID = g.AwayDivisionID
+		} else {
+			val.ClubID = g.HomeClubID
+			val.DivisionID = g.HomeDivisionID
+		}
+		standings[teamID] = val
+	}
+
+	return val
+}
+
+func updateStandingPoints(s *standing) {
+	s.Points = s.Wins * 3 + s.Ties
+}
+
+func createStandings(divisions []division, clubs []club, games []game) map[string]*standing {
+	
+	standings := map[string]*standing{}
+	for _, g := range games {
+		// Ignore games that haven't been played
+		if g.IsPlayed == "" {
+			continue
+		}
+		// initialize the home and away standing if it doesn't already exist
+		awayStanding := getStanding(standings, g, true)
+		homeStanding := getStanding(standings, g, false)
+
+		// Associate game with standing
+		awayStanding.Games = append(awayStanding.Games, g)
+		homeStanding.Games = append(homeStanding.Games, g)
+		
+		// Update wins / losses / ties
+		if g.AwayScore > g.HomeScore {
+			awayStanding.Wins++
+			homeStanding.Losses++
+		} else if g.AwayScore < g.HomeScore {
+			homeStanding.Wins++
+			awayStanding.Losses++
+		} else {
+			awayStanding.Ties++
+			homeStanding.Ties++
+		}
+
+		// Calculate Points
+		updateStandingPoints(homeStanding)
+		updateStandingPoints(awayStanding)
+	}
+
+	return standings
+}
+
+func showTeamStanding(standings map[string]*standing, teamID string) {
+	s := standings[teamID]
+	fmt.Printf("Standings: Wins: %d Losses: %d Ties: %d\n", s.Wins, s.Losses, s.Ties)
+	
+}
+
 func main() {
 
 	// Make HTTP Request
@@ -129,10 +250,11 @@ func main() {
 	clubsJSON := getJSON(scriptCode, "clubs")
 	games, clubs, divisions := parseJSONs(gamesJSON, clubsJSON, divisionsJSON)
 
-	// Print some test data
-	fmt.Printf("Game[0]: %s\nGame[1]: %s\n", games[0].ID, games[1].ID)
-	fmt.Printf("Division[0]: %s\nDivision[1]: %s\n", divisions[0].Name, divisions[1].Name)
-	fmt.Printf("Club[0]: %s\nClub[1]: %s\n", clubs[0].Name, clubs[1].Name)
-
+	// create standings data
+	standings := createStandings(divisions, clubs, games)
+	showTeamStanding(standings, "4000301")
+	showTeamStanding(standings, "4000302")
+	showTeamStanding(standings, "4000300")
+	
 	fmt.Println("Done")
 }
